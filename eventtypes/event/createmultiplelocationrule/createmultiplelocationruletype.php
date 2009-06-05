0048cd2a033620b7b614a741a4215853dd429d41
acidre <info@example.com>
2009-06-05 14:38:41 +0000

    First import
    git-svn-id: http://svn.projects.ez.no/createmultiplelocationruleevent@1 48dc52c4-8cd0-446a-9398-c16036096543

---
+++ eventtypes/event/createmultiplelocationrule/createmultiplelocationruletype.php
@@ -0,0 +1,75 @@
+﻿<?php
+/*
+
+Jean-Luc NGUYEN
+Avril 2009
+Noven.fr
+
+*/
+
+class CreateMultipleLocationRuleType extends eZWorkflowEventType
+{
+    const WORKFLOW_TYPE_STRING = "createmultiplelocationrule";
+
+    function CreateMultipleLocationRuleType()
+    {
+        $this->eZWorkflowEventType( CreateMultipleLocationRuleType::WORKFLOW_TYPE_STRING, ezi18n( 'kernel/workflow/event', 'Create Multiple Location Rule' ) );
+        $this->setTriggerTypes( array( 'content' => array( 'publish' => array( 'before' ) ) ) );
+	}
+
+    function execute( $process, $event )
+    {
+
+        $parameters = $process->attribute( 'parameter_list' );
+		$objectID = $parameters['object_id'];
+
+        $object = eZContentObject::fetch( $objectID );
+        
+        if ( is_object( $object ) )
+        	$classID = $object->attribute( 'contentclass_id' );
+    	
+    	$locationsINI = eZINI::instance( 'locations.ini' );
+    	
+    	$nodeIDArray = $locationsINI->variable( 'LocationsSettings', 'LocationsArray' );
+		$arr = array();
+		
+        $db = eZDB::instance();
+        $db->begin();
+        foreach ( array_keys( $nodeIDArray ) as $key )
+        {
+        	if ( $classID == $key ) 
+        	{
+        		$arr = explode( ';', $nodeIDArray[$key] );
+        		
+        		foreach ( $arr as $nodeID ) 
+        		{
+        			$arrNode = eZContentObjectTreeNode::fetch( $nodeID );
+        			
+        			if ( is_object( $arrNode ) ) 
+        			{
+        				$arrObject = $arrNode->attribute( 'object' );
+        				$arrClass = $arrObject->attribute( 'content_class' );
+        				$isContainer = $arrClass->attribute( 'is_container' );
+        			        				
+        				if ( $isContainer ) 
+        				{
+							// I create a new location only if this is a new created object
+							// This can be commented
+							if ( $object->attribute( 'current_version' ) == 1 ) {
+								$newNodeID = $object->addLocation( $nodeID, false );
+							}
+        				}
+        			}
+        		}
+        	}
+        }
+        $db->commit();
+
+        return eZWorkflowType::STATUS_ACCEPTED;
+
+    }
+
+}
+
+eZWorkflowEventType::registerEventType( CreateMultipleLocationRuleType::WORKFLOW_TYPE_STRING, 'CreateMultipleLocationRuleType' );
+?>
---
+++ settings/locations.ini.append.php
@@ -0,0 +1,13 @@
+<?php /*
+
+# Here you can define new locations (parent node IDs) for content classes (content class ID as array key).
+# LocationsArray[2]=44;102;19;145 - add 4 additional locations for published articles where parent node id is 44, 102, 19 and 145.
+# Note: parent class must have is_container flag set to true.
+
+[LocationsSettings]
+# chaines
+LocationsArray[42]=7317;7319;7321;7324
+# themes
+LocationsArray[48]=7318;7320;7323;7325
+
+*/ ?>
