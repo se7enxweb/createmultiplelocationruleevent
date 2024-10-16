149490e656c7f5c72f92294e7ba955b341b8efa7
7x <info@se7enx.com>
2024-10-16 20:23:01 +0000

    Added: Composer support, extension info credit files and stub README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# create multiple location rule event
+
+Adds locations to content when they are created dynamicaly via workflow event. Great for savy developers!
+
+Author: Jean-Luc NGUYEN
+
+Date: Avril 2009
+
+Company: Noven.fr
+
+License: GNU GPLv2 (or later)
+
+
---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class createmultiplelocationruleeventInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "create multiple location rule event",
+            'Version' => "1.0.0",
+            'Copyright' => "Copyright (C) 1999 - 2024 7x and 2009 Jean-Luc NGUYEN",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
